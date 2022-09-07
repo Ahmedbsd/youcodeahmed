781954b2a0b70438dd2aea718422c987ae5f060a
Ahmedbsd <112939368+Ahmedbsd@users.noreply.github.com>
2022-09-07 17:29:25 +0100

    Add files via upload

---
+++ calendrier.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	int N;
+	printf("Quel jour est octobre le ");
+	scanf("%d",&N);
+	 switch (N) {
+	
+	 case 1 : printf("Samedi");
+	       break;
+	 case 2 : printf("Dimanche");
+	       break;
+		 case 3 : printf("Lundi");
+	       break; 
+	 case 4 : printf("Mardi");
+	    break;
+     case 5 : printf("Mercredi");
+	       break;
+	 case 6 : printf("Jeudi");
+	       break;	 
+	 case 7 : printf("Vendredi");
+	       break;
+	 case 8 : printf("Samedi");
+	       break;
+	 case 9 : printf("Dimanche");
+	       break;
+		 case 10 : printf("Lundi");
+	       break; 
+	 case 11 : printf("Mardi");
+	    break;
+     case 12 : printf("Mercredi");
+	       break;
+	 case 13 : printf("Jeudi");
+	       break;	 
+	 case 14 : printf("Vendredi");
+	       break;
+	 case 15 : printf("Samedi");
+	       break;
+	 case 16 : printf("Dimanche");
+	       break;
+		 case 17 : printf("Lundi");
+	       break; 
+	 case 18 : printf("Mardi");
+	    break;
+     case 19 : printf("Mercredi");
+	       break;
+	 case 20 : printf("Jeudi");
+	       break;	 
+	 case 21 : printf("Vendredi");
+	       break;
+	 case 22 : printf("Samedi");
+	       break;
+	 case 23 : printf("Dimanche");
+	       break;
+		 case 24 : printf("Lundi");
+	       break; 
+	 case 25 : printf("Mardi");
+	    break;
+     case 26 : printf("Mercredi");
+	       break;
+	 case 27 : printf("Jeudi");
+	       break;	 
+	 case 28 : printf("Vendredi");
+	       break;
+	 case 29 : printf("Samedi");
+	       break;
+	 case 30 : printf("Dimanche");
+	       break;	
+	default : printf("cette date n'exist pas");	   	   		  
+	     break; 		   	   	   		   		            
+	 }
+	return 0;
+}
